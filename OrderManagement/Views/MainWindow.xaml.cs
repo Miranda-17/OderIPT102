75119bf8a5afc4d5ae230402f2998c51ab0d86a9
Miranda-17 <143572866+Miranda-17@users.noreply.github.com>
2026-02-28 12:32:11 +0800

    Add files via upload

---
+++ OrderManagement/Views/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using OrderManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Framework.Data;
+using Framework.Repositories;
+using Framework.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace OrderManagement.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainViewModel mainVM)
+        {
+            InitializeComponent();
+            DataContext = mainVM;
+        }
+    }
+}
